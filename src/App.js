80ebfcc0560e59a7c337b9620f753167f6e5051f
Jaffrinsampaul10 <122717589+Jaffrinsampaul10@users.noreply.github.com>
2023-03-05 23:13:43 +0530

    create file structure and config tailwind css

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code className='text-yellow-400'>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +17,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <p className='text-yellow-700'>Welcome to this demo page</p>
     </div>
   );
 }
